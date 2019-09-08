4a9de384c8a2906e1bf9f5aef3e97d1c41389bc9
Pahud <pahudnet@gmail.com>
2019-09-08 22:02:57 +0800

    minor

--- python/fargate-flask/cdk_py_samples/cdk_py_samples_stack.py
+++ python/fargate-flask/cdk_py_samples/cdk_py_samples_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core, aws_sns, aws_eks, aws_ec2, aws_iam, aws_ecs, aws_ecs_patterns
+from aws_cdk import core, aws_ec2, aws_iam, aws_ecs, aws_ecs_patterns
 
 
 class CdkPyFargateStack(core.Stack):
@@ -23,24 +23,3 @@ class CdkPyFargateStack(core.Stack):
 
         core.CfnOutput(self, 'SericeURL', value="http://{}".format(
             svc.load_balancer.load_balancer_dns_name))
-
-
-class CdkPyEksStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-
-        # import default VPC
-        vpc = aws_ec2.Vpc.from_lookup(self, 'VPC', is_default=True)
-
-        # create an admin role
-        eks_admin_role = aws_iam.Role(self, 'AdminRole',
-                                      assumed_by=aws_iam.AccountPrincipal(
-                                          account_id=self.account)
-                                      )
-
-        # create the cluster
-        cluster = aws_eks.Cluster(self, 'cluster',
-                                  cluster_name='MyEksCluster',
-                                  masters_role=eks_admin_role
-                                  )
