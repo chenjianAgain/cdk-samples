342b7b45b647798ad5383be9e90fd1db4bb8a5f2
Pahud <pahudnet@gmail.com>
2019-09-16 12:51:35 +0800

    update cross-stack samples

--- typescript/cross-stacks/bin/cross-stacks.ts
+++ typescript/cross-stacks/bin/cross-stacks.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
-import { InfraStack, FargateStack } from '../lib/cross-stacks-stack';
+import { InfraStack, FargateStack, FargateStack2 } from '../lib/cross-stacks-stack';
+import { Vpc } from '@aws-cdk/aws-ec2';
 
 const app = new cdk.App();
 
@@ -22,3 +23,12 @@ const svc = new FargateStack(app, 'FargateServiceStack', {
     env,
     vpc: infra.vpc
 })
+
+/**
+ * we can get the vpcId from the exported value from Infratack
+ */
+const svc2 = new FargateStack2(app, 'FargateServiceStack2', {
+    env,
+    vpcId: cdk.Fn.importValue('ExportedVpcId')
+})
+
--- typescript/cross-stacks/lib/cross-stacks-stack.ts
+++ typescript/cross-stacks/lib/cross-stacks-stack.ts
@@ -4,9 +4,11 @@ import ecs = require('@aws-cdk/aws-ecs');
 import ecsPatterns = require('@aws-cdk/aws-ecs-patterns');
 import { ContainerImage } from '@aws-cdk/aws-ecs';
 import { countResources } from '@aws-cdk/assert';
+import { Vpc } from '@aws-cdk/aws-ec2';
 
 export interface MyStackProps extends cdk.StackProps {
-  vpc: ec2.Vpc
+  vpc?: ec2.Vpc
+  vpcId?: string
 }
 
 export class InfraStack extends cdk.Stack {
@@ -20,21 +22,54 @@ export class InfraStack extends cdk.Stack {
     })
 
     new cdk.CfnOutput(this, 'vpcId', {
-      value: this.vpc.vpcId
+      value: this.vpc.vpcId,
+      exportName: 'ExportedVpcId'
     })
+
   }
 }
 
+/**
+ * This Fargate stack will be created within the ec2.Vpc we pass in from another stack
+ */
 export class FargateStack extends cdk.Stack {
   readonly vpc: ec2.Vpc
 
   constructor(scope: cdk.Construct, id: string, props: MyStackProps) {
     super(scope, id, props);
 
-    this.vpc = props.vpc
+    this.vpc != props.vpc
 
     const cluster = new ecs.Cluster(this, 'Cluster', {
       vpc: this.vpc
+    })
+
+    const svc = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'FargateService', {
+      cluster,
+      image: ContainerImage.fromRegistry('nginx')
+    })
+
+    new cdk.CfnOutput(this, 'ServiceURL', {
+      value: `http://${svc.loadBalancer.loadBalancerDnsName}/`
+    })
+  }
+}
+
+/**
+ * This Fargate stack will be created within the vpcId we pass in from another stack
+ */
+export class FargateStack2 extends cdk.Stack {
+  readonly vpc: string
+
+  constructor(scope: cdk.Construct, id: string, props: MyStackProps) {
+    super(scope, id, props);
+
+    this.vpc != props.vpcId
+
+    const cluster = new ecs.Cluster(this, 'Cluster', {
+      vpc: Vpc.fromLookup(this, 'Vpc', {
+        vpcId: this.vpc
+      })
     })
 
     const svc = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'FargateService', {
