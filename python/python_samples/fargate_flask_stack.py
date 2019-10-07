872db1de4598fa406cb59199dbda4e643369025c
Pahud <pahudnet@gmail.com>
2019-10-07 15:28:50 +0800

    minor fix

--- python/python_samples/fargate_flask_stack.py
+++ python/python_samples/fargate_flask_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core, aws_ec2, aws_ecs, aws_ecs_patterns
+from aws_cdk import aws_ec2, aws_ecs, aws_ecs_patterns, core
 
 
 class CdkPyFargateStack(core.Stack):
@@ -11,15 +11,23 @@ class CdkPyFargateStack(core.Stack):
 
         # ECS cluster
         cluster = aws_ecs.Cluster(self, 'Cluster', vpc=vpc)
-        svc = aws_ecs_patterns.ApplicationLoadBalancedFargateService(
-            self, 'FargateService',
-            cluster=cluster,
-            image=aws_ecs.ContainerImage.from_asset('flask-docker-app'),
-            container_port=5000,
-            environment={
-                'PLATFORM': 'AWS Fargate :-)'
-            }
-        )
+
+        task = aws_ecs.TaskDefinition(self, 'Task',
+                                      compatibility=aws_ecs.Compatibility.FARGATE,
+                                      cpu='256',
+                                      memory_mib='512'
+                                      )
+        task.add_container('flask',
+                           image=aws_ecs.ContainerImage.from_asset('flask-docker-app'),
+                           environment={
+                               'PLATFORM': 'AWS Fargate :-)'
+                           }
+                           ).add_port_mappings(5000)
+
+        svc = aws_ecs_patterns.ApplicationLoadBalancedFargateService(self, 'svc',
+                                                                     cluster=cluster,
+                                                                     task_definition=task
+                                                                     )
 
         core.CfnOutput(self, 'SericeURL', value="http://{}".format(
             svc.load_balancer.load_balancer_dns_name))
