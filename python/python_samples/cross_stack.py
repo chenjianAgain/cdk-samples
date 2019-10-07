4a1d46af42f6af93ead660e1dd221a1de882d6ea
Pahud <pahudnet@gmail.com>
2019-10-07 15:27:04 +0800

    add cross_stack python sample

--- python/python_samples/cross_stack.py
+++ python/python_samples/cross_stack.py
@@ -17,10 +17,19 @@ class CdkPyCrossStackFargateStack(core.Stack):
         self.vpc = vpc
 
         cluster = aws_ecs.Cluster(self, 'Cluster', vpc=self.vpc)
+        task = aws_ecs.TaskDefinition(self, 'Task',
+                                      compatibility=aws_ecs.Compatibility.FARGATE,
+                                      memory_mib='512',
+                                      cpu='256'
+                                      )
+
+        task.add_container('Nginx', image=aws_ecs.ContainerImage.from_registry('nginx')).port_mappings(80)
+
         svc = aws_ecs_patterns.ApplicationLoadBalancedFargateService(
             self, 'FargateService',
             cluster=cluster,
-            image=aws_ecs.ContainerImage.from_registry('nginx'))
+            task_definition=task
+        )
 
         core.CfnOutput(self, 'ServiceURL', value='http://{}/'.format(svc.load_balancer.load_balancer_full_name))
 
@@ -29,13 +38,19 @@ class CdkPyCrossStackFargateStack2(core.Stack):
     def __init__(self, scope: core.Construct, id: str, vpcId: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # self.vpc = vpcId
-
         cluster = aws_ecs.Cluster(self, 'Cluster', vpc=aws_ec2.Vpc.from_lookup(self, 'Vpc', vpc_id=vpcId))
+        task = aws_ecs.TaskDefinition(self, 'Task',
+                                      compatibility=aws_ecs.Compatibility.FARGATE,
+                                      memory_mib='512',
+                                      cpu='256'
+                                      )
+
+        task.add_container('Nginx', image=aws_ecs.ContainerImage.from_registry('nginx')).port_mappings(80)
 
         svc = aws_ecs_patterns.ApplicationLoadBalancedFargateService(
             self, 'FargateService',
             cluster=cluster,
-            image=aws_ecs.ContainerImage.from_registry('nginx'))
+            task_definition=task
+        )
 
         core.CfnOutput(self, 'ServiceURL', value='http://{}/'.format(svc.load_balancer.load_balancer_full_name))
