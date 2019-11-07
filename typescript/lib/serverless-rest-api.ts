7f9a0dc07f501c3872da1a20d377a3a952bc66ba
Pahud <pahudnet@gmail.com>
2019-11-07 16:09:30 +0800

    add another sample for serverless-rest-api

--- typescript/lib/serverless-rest-api.ts
+++ typescript/lib/serverless-rest-api.ts
@@ -1,5 +1,6 @@
 import cdk = require('@aws-cdk/core');
-import { Function, Code, Runtime, Tracing } from '@aws-cdk/aws-lambda';
+// import { Function, Code, Runtime, Tracing, InlineCode } from '@aws-cdk/aws-lambda';
+import lambda = require('@aws-cdk/aws-lambda');
 import path = require('path');
 import apigateway = require('@aws-cdk/aws-apigateway');
 import { Vpc, SubnetType } from '@aws-cdk/aws-ec2';
@@ -12,21 +13,46 @@ export class ServerlessRestApiStack extends cdk.Stack {
       isDefault: true
     })
 
-    const backend = new Function(this, 'Func', {
-      code: Code.fromAsset(path.join(__dirname, '../function/hello-world')),
+    /**
+     * Running Lambda in Python and enable the VPC support
+     */
+    const handler = new lambda.Function(this, 'FuncPython', {
+      code: lambda.Code.fromAsset(path.join(__dirname, '../function/hello-world')),
       handler: 'lambda_function.handler',
-      runtime: Runtime.PYTHON_3_7,
-      tracing: Tracing.ACTIVE,
+      runtime: lambda.Runtime.PYTHON_3_7,
+      tracing: lambda.Tracing.ACTIVE,
       memorySize: 512,
-      vpc: vpc,
+      vpc,
       vpcSubnets: {
         subnetType: SubnetType.PRIVATE
       }
     })
 
-    const api = new apigateway.LambdaRestApi(this, 'RestApi', {
-      handler: backend
+    const api = new apigateway.LambdaRestApi(this, 'PythonRestApi', {
+      handler
     })
+
+    /**
+     * Running Lambda in Node with InlineCode
+     */
+    const fn = new lambda.Function(this, 'FuncNode', {
+      code: new lambda.InlineCode(`exports.handler = (event, context, callback) =>
+      callback(null, {
+        statusCode: '200',
+        body: JSON.stringify(event),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });`),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_8_10,
+    });
+
+    new apigateway.LambdaRestApi(this, 'NodeRestAPI', {
+      handler: fn,
+      // please note in China region there's only API Gateway regional endpoint
+      endpointTypes: [apigateway.EndpointType.REGIONAL]
+    });
   }
 }
 
