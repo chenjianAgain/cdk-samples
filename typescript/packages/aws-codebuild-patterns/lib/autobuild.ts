b159ef978233f81f09906afefe11d2078e90d75d
Pahud <pahudnet@gmail.com>
2019-11-14 18:29:29 +0800

    CDK version bump to 1.16.3

--- typescript/packages/aws-codebuild-patterns/lib/autobuild.ts
+++ typescript/packages/aws-codebuild-patterns/lib/autobuild.ts
@@ -4,40 +4,89 @@ import iam = require('@aws-cdk/aws-iam');
 import codebuild = require('@aws-cdk/aws-codebuild');
 import events = require('@aws-cdk/aws-events');
 import targets = require('@aws-cdk/aws-events-targets');
-import { Alias } from '@aws-cdk/aws-kms';
 
 const NAME_PREFIX = 'autobuild'
 
-export interface ScheduledDockerBuildProps extends cdk.StackProps {
+
+export interface BaseScheduledBuildProps extends cdk.StackProps {
+  projectName?: string
   source?: codebuild.ISource
   schedule?: events.Schedule
   repositoryName?: string
   timeout?: cdk.Duration
+  ecrRepoRemovalPolicy?: cdk.RemovalPolicy
+  buildEnvironment?: codebuild.BuildEnvironment
 }
 
+export interface ScheduledBuildProps extends BaseScheduledBuildProps {
+  buildspec?: codebuild.BuildSpec
+}
+
+/**
+ * Scheduled build with the provided buildspec yaml
+ */
+export class ScheduledBuild extends cdk.Stack {
+  readonly ecrRepository: ecr.Repository
+  readonly source: codebuild.ISource
+  readonly schedule: events.Schedule
+  readonly project: codebuild.Project
+  readonly buildEnvironment?: codebuild.BuildEnvironment
+
+
+  constructor(scope: cdk.Construct, id: string, props: ScheduledBuildProps) {
+    super(scope, id, props);
+    this.ecrRepository = new ecr.Repository(this, 'Repository', {
+      repositoryName: props.repositoryName === undefined ? `${NAME_PREFIX}-${this.stackName.toLowerCase()}` : props.repositoryName,
+      removalPolicy: props.ecrRepoRemovalPolicy ? props.ecrRepoRemovalPolicy : cdk.RemovalPolicy.RETAIN
+    });
+
+    this.ecrRepository.addLifecycleRule({ maxImageAge: cdk.Duration.days(30) });
+
+    this.source = props.source === undefined ?
+      codebuild.Source.gitHub({
+        owner: 'aws',
+        repo: 'aws-cdk'
+      }) : props.source
+
+    const rule = new events.Rule(this, 'Rule', {
+      schedule: props.schedule == undefined ? events.Schedule.rate(cdk.Duration.days(1)) : props.schedule,
+    })
+
+    const project = new codebuild.Project(this, 'CodeBuildProject', {
+      // role: buildRole,
+      projectName: props.projectName,
+      source: this.source,
+      timeout: props.timeout,
+      // Enable Docker AND custom caching
+      cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER, codebuild.LocalCacheMode.CUSTOM),
+      environment: props.buildEnvironment ? props.buildEnvironment :
+        {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+          privileged: true,
+        },
+      buildSpec: props.buildspec
+    });
+    this.project = project
+    this.buildEnvironment = props.buildEnvironment
+    this.ecrRepository.grantPull(project.role!)
+    rule.addTarget(new targets.CodeBuildProject(project));
+  }
+}
+
+
+
 export class ScheduledDockerBuild extends cdk.Stack {
+  readonly projectName?: string
   readonly ecrRepository: ecr.Repository
   readonly source: codebuild.ISource
   readonly schedule: events.Schedule
 
-  constructor(scope: cdk.Construct, id: string, props: ScheduledDockerBuildProps) {
+  constructor(scope: cdk.Construct, id: string, props: ScheduledBuildProps) {
     super(scope, id, props);
     this.ecrRepository = new ecr.Repository(this, 'Repository', {
       repositoryName: props.repositoryName === undefined ? `${NAME_PREFIX}-${this.stackName.toLowerCase()}` : props.repositoryName,
+      removalPolicy: props.ecrRepoRemovalPolicy ? props.ecrRepoRemovalPolicy : cdk.RemovalPolicy.RETAIN
     });
-
-    const buildRole = new iam.Role(this, 'CodeBuildIamRole', {
-      assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com')
-    })
-    buildRole.addToPolicy(new iam.PolicyStatement({
-      resources: ['*'],
-      actions: ['ecr:GetAuthorizationToken']
-    }));
-
-    buildRole.addToPolicy(new iam.PolicyStatement({
-      resources: [`${this.ecrRepository.repositoryArn}*`],
-      actions: ['ecr:*']
-    }));
 
     this.ecrRepository.addLifecycleRule({ maxImageAge: cdk.Duration.days(30) });
 
@@ -53,7 +102,8 @@ export class ScheduledDockerBuild extends cdk.Stack {
 
 
     const project = new codebuild.Project(this, 'CodeBuildProject', {
-      role: buildRole,
+      // role: buildRole,
+      projectName: props.projectName,
       source: this.source,
       timeout: props.timeout,
       // Enable Docker AND custom caching
@@ -88,7 +138,7 @@ export class ScheduledDockerBuild extends cdk.Stack {
       })
     });
 
+    this.ecrRepository.grantPull(project.role!);
     rule.addTarget(new targets.CodeBuildProject(project));
   }
 }
-
